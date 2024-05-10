54b111aae659b8e56366fcfc770297ee02e5fb42
lucklang31 <lucklang31@gmail.com>
2024-05-10 05:35:12 +0000

    add createDict

---
+++ dict.c
@@ -0,0 +1,11 @@
+#include "dict.c"
+
+Dict* createDict(const char* name, const char* author)
+{
+    Dict* dict = (Dict*)malloc(sizeof(Dict));
+    dict->name = createCstr(name);
+    dict->author = createCstr(author);
+    dict->itemCount = 0;
+    dict->categoryCount = 0;
+}
+
